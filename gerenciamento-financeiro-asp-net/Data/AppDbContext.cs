9f6d4eba59e5f27b2acf2a42de937906df5cb15e
valeriasoars <valeriasoaressantos2@gmail.com>
2024-12-20 15:44:47 -0300

    Conexão com o Banco - SQLSERVER

---
+++ gerenciamento-financeiro-asp-net/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace gerenciamento_financeiro_asp_net.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+
+    }
+}
--- gerenciamento-financeiro-asp-net/Program.cs
+++ gerenciamento-financeiro-asp-net/Program.cs
@@ -1,7 +1,15 @@
+using gerenciamento_financeiro_asp_net.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
