1b86d21071104650bd2e67bedb70abe00c7a0daf
valeriasoars <valeriasoaressantos2@gmail.com>
2024-12-20 15:23:31 -0300

    iniciando projeto

---
+++ gerenciamento-financeiro-asp-net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gerenciamento_financeiro_asp_net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
