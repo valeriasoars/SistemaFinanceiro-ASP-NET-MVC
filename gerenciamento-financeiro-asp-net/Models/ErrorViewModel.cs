b721032935a308375a17436f4d735736c4391b2f
valeriasoars <valeriasoaressantos2@gmail.com>
2024-12-20 16:26:02 -0300

    adicionando tema bootswatch flatly

--- gerenciamento-financeiro-asp-net/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace gerenciamento_financeiro_asp_net.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- gerenciamento-financeiro-asp-net/obj/Debug/net8.0/gerenciamento-financeiro-asp-net.AssemblyInfo.cs
+++ gerenciamento-financeiro-asp-net/obj/Debug/net8.0/gerenciamento-financeiro-asp-net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gerenciamento-financeiro-asp-net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f6d4eba59e5f27b2acf2a42de937906df5cb15e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d8358d6804dbf63600efe7e014055ab666fedd")]
 [assembly: System.Reflection.AssemblyProductAttribute("gerenciamento-financeiro-asp-net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gerenciamento-financeiro-asp-net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
