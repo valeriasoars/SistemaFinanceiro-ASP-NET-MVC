43966f7eedd697bf6e6229f3b0f39d9843a85994
valeriasoars <valeriasoaressantos2@gmail.com>
2024-12-20 18:37:40 -0300

    Modelo Filtros

---
+++ gerenciamento-financeiro-asp-net/Models/Filtros.cs
@@ -0,0 +1,38 @@
+﻿namespace gerenciamento_financeiro_asp_net.Models
+{
+    public class Filtros
+    {
+        public Filtros(string filtrostring)
+        {
+            FiltroString = filtrostring ?? "todos-todos-todos" ;
+            string[] filtros = FiltroString.Split('-');
+
+            CategoriaId = filtros[0];
+            DataOperacao = filtros[1];
+            TransacaoId = filtros[2];
+        }
+
+        public string FiltroString { get; set; }
+        public string CategoriaId { get; set; }
+        public string TransacaoId { get; set; }
+        public string DataOperacao { get; set; }
+
+        public bool TemCategoria => CategoriaId.ToLower() != "todos";
+        public bool TemTransacao => TransacaoId.ToLower() != "todos";
+        public bool TemDataOperacao => DataOperacao.ToLower() != "todos";
+
+        public static Dictionary<string, string> ValoresDataOperacao =>
+            new Dictionary<string, string>
+            {
+                {"passado", "Passado" },
+                {"futuro", "Futuro" },
+                {"hoje", "Hoe" }
+            };
+
+        public bool EPassado => DataOperacao.ToLower() == "passado";
+        public bool EFuturo => DataOperacao.ToLower() == "futuro";
+        public bool EHoje => DataOperacao.ToLower() == "hoje";
+
+
+    }
+}
--- gerenciamento-financeiro-asp-net/obj/Debug/net8.0/gerenciamento-financeiro-asp-net.AssemblyInfo.cs
+++ gerenciamento-financeiro-asp-net/obj/Debug/net8.0/gerenciamento-financeiro-asp-net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gerenciamento-financeiro-asp-net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81d8358d6804dbf63600efe7e014055ab666fedd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b721032935a308375a17436f4d735736c4391b2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("gerenciamento-financeiro-asp-net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gerenciamento-financeiro-asp-net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
