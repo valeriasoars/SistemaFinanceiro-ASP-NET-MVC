d722d9f875014bb821e0a063a3472d6c1006418e
valeriasoars <valeriasoaressantos2@gmail.com>
2024-12-21 15:06:48 -0300

    View Index

--- gerenciamento-financeiro-asp-net/obj/Debug/net8.0/gerenciamento-financeiro-asp-net.AssemblyInfo.cs
+++ gerenciamento-financeiro-asp-net/obj/Debug/net8.0/gerenciamento-financeiro-asp-net.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gerenciamento-financeiro-asp-net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b721032935a308375a17436f4d735736c4391b2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f9ff8bbf419c0f40a2db5311523c0d8aa9e11c")]
 [assembly: System.Reflection.AssemblyProductAttribute("gerenciamento-financeiro-asp-net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gerenciamento-financeiro-asp-net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
